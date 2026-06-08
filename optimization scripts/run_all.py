1f6153c75ec50d4d67b58de261e02cc9649d5c07
gorazdbiskoski <gorazdbiskoskii@gmail.com>
2026-06-08 21:02:06 +0200

    Add results csvs and convergance plots for all optimizers

--- optimization scripts/run_all.py
+++ optimization scripts/run_all.py
@@ -4,10 +4,12 @@ import sys
 OPTIMIZERS = [
     "ant_colony_optimization.py",
     "bayesian_optimization_algorithm.py",
+    "genetic_algorithm.py",
     "grid_search.py",
+    "hill_climbing_algorithm.py",
+    "hill_climbing_with_simulated_annealing.py",
     "pso_optimizer.py",
     "random_search.py",
-    "genetic_algorithm.py"
 ]
 
 if __name__ == "__main__":
